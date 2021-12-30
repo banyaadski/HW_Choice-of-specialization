3742c14a3e382ffdfdaff04f12198cc9b2f6c71b
banyaadski <89918249+banyaadski@users.noreply.github.com>
2021-12-30 20:03:18 +0300

    method for create array

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int[] CreatedArray(int index, int minValue, int maxValue)
+{
+    int[] array = new int[index];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
